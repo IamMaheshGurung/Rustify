1d61b5795691d86f8afb5706c99261124fc758b0
Mahesh <gurungmahesh167@gmail.com>
2024-06-07 06:24:09 +0900

    deleted files

--- src/hello_Mahesh/src/a.rs
+++
@@ -1,6 +0,0 @@
-fn main(){
-    let mut x = 2;
-    x+=3;
-    assert_eq!(x,5);
-    println!("SUcessfull")
-}
--- src/hello_Mahesh/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    println!("Hello, world!");
-    let x:i32 = 5;
-    let _y: i32;
-    
-    assert_eq!(x,5);
-    print!("Sucess")
-}
