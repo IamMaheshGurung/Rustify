48de76c173199245b8aa70bc55aec30f6a50a583
Mahesh <gurungmahesh167@gmail.com>
2024-06-07 05:32:18 +0900

    practice

---
+++ src/hello_Mahesh/src/a.rs
@@ -0,0 +1,6 @@
+fn main(){
+    let mut x = 2;
+    x+=3;
+    assert_eq!(x,5);
+    println!("SUcessfull")
+}
---
+++ src/hello_Mahesh/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello, world!");
+    let x:i32 = 5;
+    let _y: i32;
+    
+    assert_eq!(x,5);
+    print!("Sucess")
+}
